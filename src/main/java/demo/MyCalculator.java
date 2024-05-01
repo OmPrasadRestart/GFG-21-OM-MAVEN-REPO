8120f1a52bfe77dfabe10633d6f3645d81106e35
Om Prakash Prasad <om.prasad7@gmail.com>
2024-05-01 17:50:38 +0530

    Added Multiply and Division by Om

--- src/main/java/demo/MyCalculator.java
+++ src/main/java/demo/MyCalculator.java
@@ -9,11 +9,21 @@ public class MyCalculator {
 	public int diff(int a, int b) {
 		return a-b;
 	}
+	
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	
+	public int div(int a, int b) {
+		return a/b;
+	}
 
 	public static void main(String[] args) {
 		MyCalculator calc = new MyCalculator();
 		System.out.println("Om Sum is : "+ calc.sum(20, 10));
 		System.out.println("Om Diff is : "+ calc.diff(20, 10));
+		System.out.println("Om mul is : "+ calc.mul(20, 10));
+		System.out.println("Om div is : "+ calc.div(20, 10));
 
 	}
 
